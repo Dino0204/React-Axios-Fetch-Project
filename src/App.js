68c9892418f0ab806fe04373146471580acf75d1
Dino0204 <s24020@gsm.hs.kr>
2024-10-17 20:22:48 +0900

    feat: 기본 환경설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// .then(response => response.json())   // 응답을 json으로 변경한 promise 반환 
+// .then(json => {
+//   console.log(`${json} ...fetch 데이터`)
+// })
+
+axios.get('https://jsonplaceholder.typicode.com/users/1')
+.then(response => {
+  console.log(response.data, `...axios 데이터`)
+})
+
+
+
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My - App</h1>
     </div>
   );
 }
